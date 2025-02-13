816b23e874dec5855698bde686a44f62d47ef9f7
Dyego Lima <dyegodll18@gmail.com>
2025-02-13 16:50:42 -0300

    otimizando o Generics - classe concreta DAL

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -1,17 +1,46 @@
-﻿using ScreenSound.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScreenSound.Banco
+﻿namespace ScreenSound.Banco
 {
-    internal abstract class DAL<T>
+    internal class DAL<T> where T: class
     {
-        public abstract IEnumerable<T> Listar();
-        public abstract void Adicionar(T objeto);
-        public abstract void Atualizar(T objeto);
-        public abstract void Deletar(T objeto);
+        protected readonly ScreenSoundContext _context;
+
+        protected DAL(ScreenSoundContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            var lista = _context.Set<T>().ToList();
+            lista.ForEach(a => Console.WriteLine(a + "\n"));
+            return lista;
+        }
+
+        public void Adicionar(T objeto)
+        {
+            _context.Set<T>().Add(objeto);
+            _context.SaveChanges();
+            Console.WriteLine($"{objeto} adicionado com sucesso!\n");
+        }
+
+        public void Atualizar(T objeto)
+        {
+            _context.Set<T>().Update(objeto);
+            _context.SaveChanges();
+            Console.WriteLine($"{objeto} atualizado com sucesso!\n");
+        }
+
+        public void Deletar(T objeto)
+        {
+            _context.Set<T>().Remove(objeto);
+            _context.SaveChanges();
+            Console.WriteLine($"{objeto} removido com sucesso!\n");
+        }
+
+        public T? RecuperarPor(Func<T, bool> condicao)
+        {
+            return _context.Set<T>().FirstOrDefault(condicao);
+        }
+
     }
 }
--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -9,6 +9,7 @@ internal class ScreenSoundContext: DbContext
     //propriedade ORM que se comunica com o banco relacional. Obs: deve ter o mesmo nome da tabela para que o EF identifique e associe os dois
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
+
     // caminho salvo nas propriedades da database em SQL SERVER Object Explorer
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
