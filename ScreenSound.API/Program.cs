7aa4887cd69a365a2ab558b578dff0562301bbb6
Dyego Lima <dyegodll18@gmail.com>
2025-02-19 07:42:44 -0300

    Injeção de Dependências [FromServices] recurso nativo do Asp.NET que permite que ele crie os objetos que vamos utilizar na aplicação.

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -5,21 +5,30 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Injeção de dependências
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 //resolve erro de referência cíclica entre Artista e Música
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-app.MapGet("/Artistas", () => 
+/*
+    A partir da configuração via builder.Services, falamos que todos os nossos 
+    endpoints dependem de um serviço para funcionar, que definimos utilizando o 
+    atributo [FromService] para invocar o objeto DAL que vai ser criado pela 
+    própria aplicação. 
+*/
+
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) => 
 {
-    var artistaDAL = new DAL<Artista>(new ScreenSoundContext());
-    return Results.Ok(artistaDAL.Listar());
+    return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) => 
+app.MapGet("/Artistas/{nome}", ([FromServices]DAL<Artista> dal, string nome) => 
 {
-    var artistaDAL = new DAL<Artista>(new ScreenSoundContext());
-    var artista = artistaDAL.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista is null) return Results.NotFound();
     return Results.Ok(artista);
 });
